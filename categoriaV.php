81d0f70a60a0921e72e1818cbc6f356984b3233a
root <root@itachi>
2016-04-24 10:19:32 -0600

    Clase 24 de abril

--- categoriaV.php
+++ categoriaV.php
@@ -11,6 +11,57 @@
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
     </head>
     <body>
+        <form method="post">
+            <input type="search" required="true" placeholder="Buscar" name="buscar"><button type="submit" name="btnBuscar">Buscar</button>
+        </form>
+        <br><br>
+        <?php
+        include_once('controlador/CategoriaC.php');
+        if(isset($_GET["delete"])){
+            $c = new CategoriaC();
+            $c->delete($_GET["delete"]);
+        }
+        
+        if (isset($_GET["id"])) {
+            $cc = new CategoriaC();
+            $dato = $cc->busqueda($_GET["id"]);
+            foreach ($dato as $d) {
+                ?>
+
+                <form action="" method="post">
+                    <input type="hidden" name="id" value="<?= $d->idCategoria ?>">
+                    <p><label for="nombre">Nombre</label><input type="text" value="<?= $d->nombre ?>" name="nombre" id="nombre" required="true" placeholder="Nombre" ></p>
+                    <p><label for="desc">Descripcion</label><textarea name="desc" id="desc" placeholder="Descripcion"><?= $d->descripcion ?></textarea></p>
+                    <p><button type="submit" name="guardar">Modificar</button></p>
+                </form>
+                <?php
+            }
+        } else {
+            ?>
+            <form action="" method="post">
+                <p><label for="nombre">Nombre</label><input type="text" name="nombre" id="nombre" required="true" placeholder="Nombre" ></p>
+                <p><label for="desc">Descripcion</label><textarea name="desc" id="desc" placeholder="Descripcion"></textarea></p>
+                <p><button type="submit" name="crear">Crear</button></p>
+            </form>
+            <?php
+        }
+        ?>
+
+
+        <?php
+        if (isset($_POST["nombre"]) && isset($_POST["crear"])) {
+            $nombre = htmlentities($_POST["nombre"]);
+            $desc = htmlentities($_POST["desc"]);
+            $c = new CategoriaC();
+            $c->add(array("nombre" => $nombre, "desc" => $desc));
+        } else if (isset($_POST["nombre"]) && isset($_POST["guardar"])) {
+            $nombre = htmlentities($_POST["nombre"]);
+            $desc = htmlentities($_POST["desc"]);
+            $id = htmlentities($_POST["id"]);
+            $c = new CategoriaC();
+            $c->update(array("nombre" => $nombre, "desc" => $desc, "id" => $id));
+        }
+        ?>
         <table class="table table-responsive">
             <thead>
                 <tr>
@@ -18,13 +69,12 @@
                     <th>Nombre </th>
                     <th>Fecha Creación</th>
                     <th>Fecha Modificación</th>
-                    <th>Modificar<th>
+                    <th>Modificar</th>
                     <th>Borrar</th>
                 </tr>
             </thead>
             <tbody>
                 <?php
-                include_once('controlador/CategoriaC.php');
                 $cat = new CategoriaC();
                 foreach ($cat->getAll() as $c) {
                     ?>
@@ -33,13 +83,13 @@
                         <td><?= $c->nombre ?></td>
                         <td><?= $c->fecha_creacion ?></td>
                         <td><?= $c->fecha_mod ?></td>
-                        <td><a href=""><span class="glyphicon glyphicon-pencil"><span></a></td>
-                                        <td><a href=""><span class="glyphicon glyphicon-trash"></span></a></td>
-                    </tr>
-                    <?php
-                }
-                ?>
-            </tbody>
-        </table>
-    </body>
-</html>
+                        <td><a href="categoriaV.php?id=<?= $c->idCategoria ?>"><span class="glyphicon glyphicon-pencil"><span></a></td>
+                                        <td style="text-align: center;"><a href="categoriaV.php?delete=<?= $c->idCategoria ?>"><span class="glyphicon glyphicon-trash"></span></a></td>
+                                        </tr>
+                                        <?php
+                                    }
+                                    ?>
+                                    </tbody>
+                                    </table>
+                                    </body>
+                                    </html>
